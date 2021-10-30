4ed358155b7af17912e7999bdb7ad72fa89fea18
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-10-30 17:13:13 +0300

    file

--- #src/js/index/index.js
+++ #src/js/index/index.js
@@ -19,3 +19,110 @@ var indexShopSlider = new Swiper(".indexShopSlider", {
     slidesPerView: 5,
     spaceBetween: 40,
 });
+var indexHeaderSlider = new Swiper(".indexHeaderSlider", {
+    navigation: {
+        nextEl: ".ind-header__content  .swiper-controls .swiper-button-next",
+        prevEl: ".ind-header__content  .swiper-controls .swiper-button-prev",
+    },
+    pagination: {
+        el: ".ind-header__content .swiper-controls .swiper-pagination",
+    },
+    mousewheel: true,
+    effect: "fade",
+    fadeEffect: { 
+        crossFade: true 
+    },
+});
+
+
+$('#indexShopButton-1').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-1').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-2').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-2').removeClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-3').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-3').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-4').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-4').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-5').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-5').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+});
+
+
+
+let activeContentIndex = 0
+let elementContentCount = 4
+
+$('#indexCatalogNext').on('click', function() {
+    changeSlide('next')
+})
+
+$('#indexCatalogPrev').on('click', function() {
+    changeSlide('prev')
+})
+
+function changeSlide(id) { 
+    if(id === 'next') {
+        activeContentIndex++
+        if (activeContentIndex === elementContentCount) {
+            activeContentIndex = 0
+        } 
+    } else if (id === 'prev') {
+        activeContentIndex--
+        if (activeContentIndex < 0) {
+            activeContentIndex = elementContentCount - 1
+        }
+    }
+    console.log(activeContentIndex)
+    if(activeContentIndex == 0) {
+
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,79 @@ var indexShopSlider = new Swiper(".indexShopSlider", {
     slidesPerView: 5,
     spaceBetween: 40,
 });
+var indexHeaderSlider = new Swiper(".indexHeaderSlider", {
+    navigation: {
+        nextEl: ".ind-header__content  .swiper-controls .swiper-button-next",
+        prevEl: ".ind-header__content  .swiper-controls .swiper-button-prev",
+    },
+    pagination: {
+        el: ".ind-header__content .swiper-controls .swiper-pagination",
+    },
+    mousewheel: true,
+    effect: "fade",
+    fadeEffect: { 
+        crossFade: true 
+    },
+});
+
+
+$('#indexShopButton-1').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-1').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-2').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-2').removeClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-3').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-3').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-4').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-5').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-4').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+    $('#indexShopElement-5').addClass('display-n')
+});
+$('#indexShopButton-5').on('click', function() {
+    $(this).addClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-2').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-3').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-4').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopButton-1').removeClass('ind-shop__block-list-item-active')
+    $('#indexShopElement-5').removeClass('display-n')
+    $('#indexShopElement-2').addClass('display-n')
+    $('#indexShopElement-3').addClass('display-n')
+    $('#indexShopElement-4').addClass('display-n')
+    $('#indexShopElement-1').addClass('display-n')
+});
